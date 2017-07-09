63a7c76d81a214aceba281ff3f803ac6b2a9ecfe
jammy <derpy.l.llama@gmail.com>
2017-07-09 17:11:42 +0800

    HttpRequestMethod enum

---
+++ Webserver/Http/HttpRequestMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jambox.Web.Http
+{
+    enum HttpRequestMethod
+    {
+        GET,
+        HEAD,
+        POST,
+        PUT,
+        DELETE,
+        TRACE,
+        CONNECT,
+        OPTIONS
+    }
+}
